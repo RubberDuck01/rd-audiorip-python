a1874f256876a9985e220748d00ec574d1530c6a
RubberDuck01 <spehar.luka@hotmail.com>
2026-04-29 22:17:57 +0200

    tweak stats messages

--- source/rd_audiorip/ui/stats_dialog.py
+++ source/rd_audiorip/ui/stats_dialog.py
@@ -23,7 +23,7 @@ class StatsDialog(QDialog):
         stats_form.addRow("Sessions:", QLabel(str(self.stats.data.get("total_sessions", 0))))
         stats_form.addRow("Tracks downloaded:", QLabel(str(self.stats.data.get("total_files_downloaded", 0))))
         stats_form.addRow("Total size:", QLabel(self.stats.data.get("total_downloads_size_pretty", "0 B")))
-        stats_form.addRow("Downloaded tracks combined duration:", QLabel(self.stats.data.get("total_downloads_duration_pretty", "00:00")))
+        stats_form.addRow("Total duration (combined):", QLabel(self.stats.data.get("total_downloads_duration_pretty", "00:00")))
         layout.addWidget(stats_group)
 
         # Timeline group
@@ -31,8 +31,8 @@ class StatsDialog(QDialog):
         timeline_form = QFormLayout(timeline_group)
         timeline_form.setContentsMargins(10, 12, 10, 10)
         timeline_form.setSpacing(6)
-        timeline_form.addRow("Profile created:", QLabel(self.format_date(self.stats.data.get("created_date", "N/A"))))
         timeline_form.addRow("Last run:", QLabel(self.format_date(self.stats.data.get("last_run_date", "N/A"))))
+        timeline_form.addRow("Profile created:", QLabel(self.format_date(self.stats.data.get("created_date", "N/A"))))
         layout.addWidget(timeline_group)
 
         layout.addStretch()
